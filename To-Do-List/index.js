1c161807cf171634400a7a3be7685f90b423f703
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-02-01 12:26:03 +0530

    Final Commit

--- To-Do-List/index.js
+++ To-Do-List/index.js
@@ -1,12 +1,18 @@
+function addTask(){
+    let item = document.getElementById('box');
+    let list_item = document.getElementById('l-item');
+    if(item.value != ""){
+        let make_li = document.createElement("li");
+        make_li.appendChild(document.createTextNode(item.value));
+        list_item.appendChild(make_li);
+        item.value="";
 
-function addToList(){
-    let pt1 = document.getElementById("pt1");
-    let pt2 = document.getElementById("pt2");
-    let ft = document.getElementById("task");
-    let pty1 = document.getElementById("imp-urg");
-    let pty = document.getElementsByName("priority");
-    // console.log(ft.value);
-    // console.log(pty[0].input.value);
-    pt1.innerHTML = ft.value;
-    // pt2.innerHTML = pty.value;
+        make_li.ondblclick=function(){
+            this.parentNode.removeChild(this);
+        }
+    }
+    else{
+        alert("Please enter the task in the textbox")
+    }
 }
+
